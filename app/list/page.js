707172678b7e7d623c9529d7b4fd976739b91d4c
김영준 <dudwns1ms1234@gmail.com>
2026-01-06 15:06:39 +0900

    monggodb to list page

---
+++ app/list/page.js
@@ -0,0 +1,22 @@
+import { connectDB } from "@/util/database"
+
+export default async function List() {
+
+    const db = (await connectDB).db("forum")
+    let result = await db.collection('post').find().toArray()
+    console.log(result)
+    return (
+        <>
+            <div className="list-bg">
+                {
+                    result.map((a, i) =>
+                        <div className="list-item" key={i}>
+                            <h4>{a.title}</h4>
+                            <p>{a.content}</p>
+                        </div>
+                    )
+                }
+            </div>
+        </>
+    )
+}
